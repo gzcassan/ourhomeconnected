d3a337a1c6eb79ac3b3e06f54b96e033118f742f
Daniel de Jong <danieldotnl@outlook.com>
2018-02-18 17:41:45 +0100

    Start overthrow to use WhenDoJobs. Add PhilipsHue command handler for WhenDoJobs.

---
+++ OHC.WhenDoJobs.CommandHandlers/PhilipsHueCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using OHC.Drivers.PhilipsHue;
+using System;
+using System.Threading.Tasks;
+
+namespace OHC.WhenDoJobs.CommandHandlers
+{
+    public class PhilipsHueCommandHandler
+    {
+        private IPhilipsHueClient client;
+
+        public PhilipsHueCommandHandler(IPhilipsHueClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task SwitchOnAsync(string area)
+        {
+            await client.SwitchOnOffAsync(true, area);
+        }
+
+        public async Task SwitchOffAsync(string area)
+        {
+            await client.SwitchOnOffAsync(false, area);
+        }
+
+        public async Task SwitchOnScene(string scene, string area)
+        {
+            await client.SwitchToSceneAsync(scene, area);
+        }
+    }
+}
